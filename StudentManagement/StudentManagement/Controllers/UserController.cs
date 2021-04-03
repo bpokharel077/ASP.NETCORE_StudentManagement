cc0d72c92093b68540c67f33574d2fbf9f1eabd6
bishalpokharel@ambition.edu.np <bishalpokharel@ambition.edu.np>
2021-04-03 10:26:20 +0545

    hello world added

---
+++ StudentManagement/StudentManagement/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("hello world");
+            return View();
+            
+        }
+    }
+}
