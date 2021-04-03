a2c5bd3ed037ff4267aed38c751aa3640fd41540
gauravpokharel@ambition.edu.np <gauravpokharel@ambition.edu.np>
2021-04-03 09:20:01 +0545

    Added User Model

---
+++ StudentManagement/StudentManagement/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class UserModel
+    {
+    }
+}
